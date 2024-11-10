90c682463092722c2752b714f301ee7a56ec784f
anilsinthu114 <anilsinthu114@gmail.com>
2024-11-10 13:36:36 +0530

    added Analytics and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,33 @@
 import '../styles/globals.css';
 import { motion } from 'framer-motion';
+import {Analytics} from '@vercel/analytics/react'
+import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
+/**
+ * The App component is the top-level component of the application.
+ * It wraps the entire application in a motion.div for smooth transitions.
+ * It also includes the Analytics component for tracking and analysis.
+ * 
+ * @param {Object} Component - The component to be rendered.
+ * @param {Object} pageProps - The props passed to the page component.
+ * @returns {JSX.Element} The App component.
+ */
 export default function App({ Component, pageProps }) {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Component {...pageProps} />
-    </motion.div>
+    <>
+      <Head>
+        <link rel="icon" href="/path/to/your/favicon.ico" type="image/x-icon" />
+        <meta name="description" content="Your website description here." />
+      </Head>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Component {...pageProps} />
+        <Analytics />
+      </motion.div>
+    </>
   );
 }
